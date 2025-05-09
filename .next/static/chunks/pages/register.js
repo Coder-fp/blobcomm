73b5e6afdd29ecd34791c6a700a0a7db5cab966a
Coder-fp <freddie.powell197@gmail.com>
2025-05-09 18:31:13 +0100

    Delete .next directory

--- .next/static/chunks/pages/register.js
+++
@@ -1,9 +0,0 @@
-__turbopack_load_page_chunks__("/register", [
-  "static/chunks/node_modules_next_63387682._.js",
-  "static/chunks/node_modules_react-dom_82bb97c6._.js",
-  "static/chunks/node_modules_2a5cf4bb._.js",
-  "static/chunks/[root-of-the-server]__d06051ca._.js",
-  "static/chunks/styles_Register_module_33487f64.css",
-  "static/chunks/pages_register_5771e187._.js",
-  "static/chunks/pages_register_2bf1835e._.js"
-])
